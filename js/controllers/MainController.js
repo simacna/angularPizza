705c1b4e87720a9aadb2f365388d735099bb5a47
simacna <simacna@gmail.com>
2015-03-01 11:07:44 -0500

    first commit

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -11,7 +11,51 @@ app.controller('MainController', ['$scope', function($scope) {
       name: 'Mozzarella Sticks',
       description: 'Served with marinara sauce.',
       price: 3.95
+    },
+    {
+      name: 'Bruschetta',
+      description: 'Grilled bread garlic, tomatoes, olive oil',
+      price: 4.95
     }
   ];
+
+  $scrope.mains = [
+    {
+      name: 'Margherita',
+      description: 'Marinara sauce, mozzarella, fresh basil',
+      price: 6.95
+    },
+    {
+      name: 'Pepperoni',
+      description: 'Pepperoni, marinara sauce, mozzarella, fresh oregano',
+      price: 7.95
+    },
+    {
+      name: 'Rusitica',
+      description: 'Marina sauce, kalamata olivces, fresh oregano',
+      price: 8.95
+    }
+
+  ];
+
+  $scope.extras = [
+    {
+      name: 'Breadsticks',
+      description: 'Served with marinara sauce',
+      price: 4.95
+    },
+    {
+      name: 'Soup of the Day',
+      description: 'Take a chance',
+      price: 4.95
+    },
+    {
+      name: 'Buffalo Wings',
+      description: 'Feel the power of wings',
+      price: 6.95
+    }
+
+  ];
+  ]
 
 }]);
